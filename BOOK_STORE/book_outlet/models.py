4d9a8faa8baaa6d7c5afdf64989973c84ae88d0b
JasimUllah <jasimullah3@gmail.com>
2024-11-26 14:31:35 +0500

    Remove folder

--- BOOK_STORE/book_outlet/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Book(models.Model):
-    title = models.CharField(max_length=50)   # The max_length argument is must to pass to CharField constructor
-    rating = models.IntegerField()
