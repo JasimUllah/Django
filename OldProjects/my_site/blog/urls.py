4ffb5ff944a8ffd3e163a70a57bd2961150a6564
Jasim Ullah <88829152+JasimUllah@users.noreply.github.com>
2024-12-11 14:51:14 +0500

    Delete OldProjects/my_site directory

--- OldProjects/my_site/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views  # dot represents the current folder
-
-urlpatterns = [
-    path("", views.starting_page, name="starting-page"),   # path for empty page
-    path("posts", views.posts, name="posts-page"),  # path for posts page
-    path("posts/<slug:slug>", views.post_detail, name="posts-detail-page")
-]
