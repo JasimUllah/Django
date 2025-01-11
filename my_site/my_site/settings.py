5ecc24a66713e4fa34b0101946891dc130b4220c
JasimUllah <jasimullah3@gmail.com>
2025-01-11 23:14:29 +0500

    settings

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hswue5_db($3o8#@_u$209j@k3wqvp=pb52ey1r2&ldy+v74i7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
